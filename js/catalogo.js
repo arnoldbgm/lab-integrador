7f7d26d76df5f0bf79b69c74ab67d05cd00b25c6
gonzalo <gonzalomogollon12345@gmail.com>
2026-01-14 20:55:35 -0500

    HU:02-03 Finalizado

--- js/catalogo.js
+++ js/catalogo.js
@@ -2,6 +2,7 @@
 let inptTituloLibro = document.querySelector("#tituloLibro");
 let inptAutorLibro = document.querySelector("#autorLibro");
 let inptIsbnLibro = document.querySelector("#isbnLibro");
+let tbBodyLibros = document.querySelector("#TablaLibros");
 
 let btnAgregarLibro = document.querySelector("#btnAgregarLibro");
 
@@ -42,4 +43,30 @@ function agregarLibro() {
    inptIsbnLibro.value = "";
 
    alert("Libro agregado exitosamente")
+
+   mostrarLibros();
+}
+
+function mostrarLibros() {
+   tbBodyLibros.innerHTML = "";
+   librosDisponibles.forEach( libro => {
+     tbBodyLibros.innerHTML += `
+       <tr>
+         <td class="border p-2">${libro.titulo}</td>
+          <td class="border p-2">${libro.autor}</td>
+          <td class="border p-2">${libro.isbn}</td>
+          <td class="border p-2">${libro.estado}</td>
+          <td class="border p-2">
+            <button class="bg-red-500 text-white px-2" onclick="eliminarLibro(${index})">Eliminar</button>
+          </td>
+       </tr>
+     `;
+   });
+}
+
+function eliminarLibro(index) {
+   alert(`libro con indice ${index} eliminado`);
+   librosDisponibles.splice(index, 1);
+   localStorage.setItem("librosDisponibles", JSON.stringify(librosDisponibles));
+   mostrarLibros();
 }
